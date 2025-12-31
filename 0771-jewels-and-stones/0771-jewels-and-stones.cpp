2ef9151b85636644291a63da29b1a009d19d7326
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2025-12-31 20:22:00 +0530

    Time: 3 ms (12.24%), Space: 8.8 MB (23.51%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_map<char, int> mp;
+        int count = 0;
+
+        for(char ch : stones) {
+            mp[ch]++;
+        }
+
+        for(char i : jewels) {
+            if(mp[i] > 0) {
+                count += mp[i];
+            }
+        }
+        return count;
+    }
+};
