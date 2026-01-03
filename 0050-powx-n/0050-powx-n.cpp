16c3c73da29889a6d3d0a1776c295a9acc7259fd
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-01-03 15:31:45 +0530

    Time: 0 ms (100%), Space: 8.5 MB (91.73%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    double solve(double x, long long n) {
+    
+        if (n == 0)
+            return 1;
+
+        
+        double half = solve(x, n / 2);
+
+        
+        if (n % 2 == 0)
+            return half * half;
+        else
+            return half * half * x;
+    }
+
+    double myPow(double x, int n) {
+        long long power = n;   
+
+        if (power < 0) { //He mi visartooo
+            x = 1 / x;
+            power = -power;
+        }
+
+        return solve(x, power);
+    }
+};
