1cc0fd09ffbb5ec62ac67a3fc94ee54b408b88c8
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-01-03 16:10:21 +0530

    Time: 0 ms (100%), Space: 8.3 MB (99.2%) - LeetHub

--- 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int mirrorDistance(int n) {
-        int vriginal = n;
+        int vriginal = n; //Return to store this then compute...Ithe mi chukto
+
         int rev = 0;
 
     while (n != 0) {
@@ -14,3 +15,7 @@ public:
         return abs(vriginal-rev);
     }
 };
+
+// string str = to_string(n);
+//         reverse(str.begin(),str.end());
+//         return abs(stoi(str)-n);
