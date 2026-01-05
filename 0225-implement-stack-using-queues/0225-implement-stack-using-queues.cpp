fc4e2a9092ee3e178f24be85d53b874de6cf4c87
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-01-05 09:31:08 +0530

    Time: 0 ms (100%), Space: 9.5 MB (54.91%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,36 @@
+class MyStack {
+    queue<int> q1, q2;
+
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        // Push new element into q2
+        q2.push(x);
+
+        // Move all elements from q1 to q2
+        while (!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+
+        // Swap q1 and q2
+        swap(q1, q2);
+    }
+    
+    int pop() {
+        int val = q1.front();
+        q1.pop();
+        return val;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
