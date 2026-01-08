f372b0b49c60c09d1228f736828704ae379ed85f
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-01-08 19:49:58 +0530

    Time: 39 ms (73.4%), Space: 45.5 MB (40.31%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,23 +1,66 @@
+// class Solution {
+// public:
+    // int subarraySum(vector<int>& nums, int k) {
+    //     int count = 0;
+    //     int n = nums.size();
+
+    //     for(int i =0;i<n;i++)
+    //     {
+    //         int sum = 0;
+    //         for(int j =i;j < n;j++)
+    //         {
+    //             sum  = sum + nums[j];
+    //             if(sum ==k)
+    //             {
+    //                 count++;
+    //             }
+    //         }
+    //     }
+    //     return count;
+
+        
+    // }
+
+    // {
+    //     int ans = 0, n = nums.size();
+    //     unordered_map<int, int> mp;
+    //     int sum=0;
+    //     for(int i=0; i<n; i++){
+    //         mp[sum]++;
+    //         sum+=nums[i];
+    //         int rem = sum-k;
+
+    //         if(mp.find(rem) != mp.end()){
+    //             ans+=mp[rem];
+    //         }
+
+    //     }
+
+       
+
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int count = 0;
-        int n = nums.size();
+        int ans = 0, n = nums.size();
+        unordered_map<int, int> mp;
+        int sum=0;
+        for(int i=0; i<n; i++){
+            mp[sum]++;
+            sum+=nums[i];
+            int rem = sum-k;
 
-        for(int i =0;i<n;i++)
-        {
-            int sum = 0;
-            for(int j =i;j < n;j++)
-            {
-                sum  = sum + nums[j];
-                if(sum ==k)
-                {
-                    count++;
-                }
+            if(mp.find(rem) != mp.end()){
+                ans+=mp[rem];
             }
-        }
-        return count;
 
-        
+        }
+
+       
+
+        return ans;
     }
 };
