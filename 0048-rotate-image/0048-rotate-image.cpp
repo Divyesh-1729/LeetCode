cf408cd23ea1dd7c5a95e64a59a4cfacb5bbaf1d
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-01-08 20:27:01 +0530

    Time: 0 ms (100%), Space: 10.1 MB (91.64%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,30 @@
+// Brute-force: use extra matrix B and map A[i][j] -> B[j][n-1-i]
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& A) {
+        // int n = A.size();
+        // if (n <= 1) return;
+        // vector<vector<int>> B(n, vector<int>(n));
+        // for (int i = 0; i < n; ++i) {
+        //     for (int j = 0; j < n; ++j) {
+        //         B[j][n - 1 - i] = A[i][j];
+        //     }
+        // }
+        // // copy back
+        // for (int i = 0; i < n; ++i)
+        //     for (int j = 0; j < n; ++j)
+        //         A[i][j] = B[i][j];
+
+        reverse(A.begin(), A.end());
+        
+        for (int i = 0; i < A.size(); ++i) {
+            for (int j = i + 1; j < A[i].size(); ++j) {
+                swap(A[i][j], A[j][i]);
+            }
+        }
+    
+    }
+};
