cc8e68d33b713dc057962a08d4f1b2d4ffc4beaf
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-01-17 12:44:12 +0530

    Time: 0 ms (100%), Space: 8.2 MB (46.99%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,25 +1,57 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int low = 0, high = 0;
+        // int low = 0, high = 0;
 
-        for (char c : s) {
-            if (c == '(') {
-                low++;
-                high++;
+        // for (char c : s) {
+        //     if (c == '(') {
+        //         low++;
+        //         high++;
+        //     }
+        //     else if (c == ')') {
+        //         low--;
+        //         high--;
+        //     }
+        //     else {
+        //         low--;
+        //         high++;
+        //     }
+
+        //     if (high < 0) return false;
+        //     if (low < 0) low = 0;
+        // }
+        // return low == 0;
+
+        stack<int> open;
+        stack<int> star;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                open.push(i);
             }
-            else if (c == ')') {
-                low--;
-                high--;
+            else if (s[i] == '*') {
+                star.push(i);
             }
             else {
-                low--;
-                high++;
+                if (!open.empty()) {
+                    open.pop();
+                }
+                else if (!star.empty()) {
+                    star.pop();
+                }
+                else {
+                    return false;
+                }
             }
-
-            if (high < 0) return false;
-            if (low < 0) low = 0;
         }
-        return low == 0;
+
+        while (!open.empty() && !star.empty()) {
+            if (open.top() > star.top()) return false;
+            open.pop();
+            star.pop();
+        }
+
+        return open.empty();
+
     }
 };
