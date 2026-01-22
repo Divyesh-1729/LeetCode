50d4772552c2008cf946ecc52e317b0b3869461b
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-01-22 10:45:57 +0530

    Time: 0 ms (100%), Space: 15.8 MB (31.99%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,34 @@
+#include<algorithm>
+#include <vector>
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        //next_permutation(nums.begin(),nums.end());
+
+       
+
+        int idx = -1;
+
+ 
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                idx = i;
+                break;
+            }
+        }
+
         
+        if (idx != -1) {
+            for (int i = nums.size() - 1; i > idx; i--) {
+                if (nums[i] > nums[idx]) {
+                    swap(nums[i], nums[idx]);
+                    break;
+                }
+            }
+        }
+
+        
+        reverse(nums.begin() + idx + 1, nums.end());
     }
 };
+
