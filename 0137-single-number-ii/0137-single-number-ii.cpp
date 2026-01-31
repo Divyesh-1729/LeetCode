7f3e77605f72de25f7fb8f814ec7fd622ef37728
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-01-31 23:06:59 +0530

    Time: 0 ms (100%), Space: 14.2 MB (10.68%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> freq;
+        unordered_map<int, int>freq;
 
-        for (int x : nums) 
+        for (int x :nums) 
         {
             freq[x]++;
         }
 
-        for (auto &p : freq) 
+        for (auto &p :freq) 
         {
-            if (p.second == 1)
+            if (p.second==1)
             {
                 return p.first;
             }
         }
-
         return 0;
     }
 };
