fa2a87da693e46c2f2c29eaf1f674e0c4c163be7
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-03-01 17:18:36 +0530

    Time: 0 ms (100%), Space: 8.1 MB (19.08%) - LeetHub

---
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(int n) {
+        // vector<int>nums(n,0);
+        // for(int i = 0;i<n;i++)
+        // {
+        //     nums[i] = (2 * i) + 1;
+        // }
+        // int x = 0;
+        // int sum = 0;
+        // for(int i : nums)
+        // {
+        //     sum = sum + i;
+        // }
+        // int avg = sum/n;
+        // int y= nums.size()-1;
+        // while(x<y)
+        // {
+
+        // }
+        return (n*n)/4;
+    }
+};
