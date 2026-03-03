8e1f7186d4746fc00aa2a17c50184ae2cb7145be
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-03-04 00:52:20 +0530

    Time: 1523 ms (17.21%), Space: 483.9 MB (6.88%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
@@ -1,12 +1,16 @@
 class Solution {
     public:
         int countCompleteSubarrays(vector<int>& nums) {
-            int k = unordered_set<int>(nums.begin(),nums.end()).size(), res = 0;
+            int k = unordered_set<int>(nums.begin(),nums.end()).size();
+            int res = 0;
             for (int i = 0; i < nums.size(); i++) {
                 unordered_set<int> st;
                 for (int j = i; j < nums.size(); j++) {
                     st.insert(nums[j]);
-                    if (st.size() == k) res++;
+                    if (st.size() == k) 
+                    {
+                        res = res +1;
+                    }
                 }
             }
             return res;
