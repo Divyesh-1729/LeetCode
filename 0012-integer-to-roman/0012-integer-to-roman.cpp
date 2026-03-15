c4ce0f2491240ce4844ca4b153ea9fdc546001be
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-03-15 10:01:16 +0530

    Time: 2 ms (70.45%), Space: 9.2 MB (86.9%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -7,12 +7,12 @@ public:
         string sym[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         
-        for(int i=0; num != 0; i++)
+        for(int i = 0; i < 13; i++) 
         {
-            while(num >= val[i])
+            while(num >= val[i]) 
             {
-                num -= val[i];
                 res += sym[i];
+                num -= val[i];
             }
         }
         
