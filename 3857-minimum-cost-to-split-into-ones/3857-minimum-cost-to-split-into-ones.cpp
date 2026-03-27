f1f10ef8c5de8710afb8a30fe3585e9c179b3c9d
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-03-27 14:44:55 +0530

    Time: 82 ms (5.34%), Space: 10.4 MB (11.19%) - LeetHub

--- 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
@@ -1,8 +1,32 @@
 class Solution {
 public:
+int solve(int n,vector<int>& dp)
+{
+    if(n==1)
+    {
+        return 0;
+    }
+    if(dp[n]!=-1)
+    {
+        return dp[n];
+    }
+    int mini = INT_MAX;
+    for(int i=1;i<=n/2;i++)
+    {
+            int a=i;
+            int b =n-i;
+            int cost = a * b+solve(a,dp) +solve(b,dp);
+
+            mini = min(mini,cost);
+
+    }
+    return dp[n]=mini;
+}
 
 
     int minCost(int n) {
-        return n*(n-1)/2;
+        vector<int>dp(n+10,-1);
+        int ans = solve(n,dp);
+        return ans;
     }
 };
