a22a4b40e8e061280f8fd3cd417fcbe80e25bfd4
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-04-05 21:51:05 +0530

    Time: 0 ms (100%), Space: 10.4 MB (13.45%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        int ud = 0, lr = 0;
+        int ud = 0;
+        int lr = 0;
 
-        for (char c : moves) {
-            if (c == 'U') 
-            {
-                ud++;
-            }
-            else if (c == 'D') 
-            {
-                ud--;
-            }
-            else if (c == 'L') 
-            {
-                lr--;
-            }
-            else if (c == 'R') 
+        for(char c:moves)
+        {
+            if(c=='R')
             {
                 lr++;
             }
+            if(c=='L')
+            {
+                lr--;
+            }
+            if(c=='U')
+            {
+                ud++;
+            }
+            if(c=='D')
+            {
+                ud--;
+            }
         }
-
-        if(ud == 0 && lr == 0)
+        if(ud==0 && lr==0)
         {
             return true;
         }
