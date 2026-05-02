b6d76633dcf99086eeb709444633c1c7dee4287b
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-05-02 15:47:05 +0530

    Time: 0 ms (100%), Space: 7.9 MB (46.67%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isgood(int num)
+    {
+        bool changed = false;
+        while(num>0)
+        {
+            int digit = num%10;
+            if(digit == 2||digit == 5||digit == 6||digit == 9)
+            {
+                changed = true;
+            }
+            else if(digit == 3||digit == 4||digit == 7)
+            {
+                return false;
+            }
+            num =num/10;
+        }
+        return changed;
+    }
+    int rotatedDigits(int n) {
+        int count  = 0;
+        for(int i =1;i<=n;i++)
+        {
+            if(isgood(i))
+            {
+                count++;
+            }
+        }
+        return count;
+
+    }
+};
