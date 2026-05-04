11a01953b31a1972cb29cf63bacd908be6767029
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-05-04 09:12:25 +0530

    Time: 0 ms (100%), Space: 10.3 MB (35.34%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,30 +1,14 @@
-// Brute-force: use extra matrix B and map A[i][j] -> B[j][n-1-i]
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    void rotate(vector<vector<int>>& A) {
-        // int n = A.size();
-        // if (n <= 1) return;
-        // vector<vector<int>> B(n, vector<int>(n));
-        // for (int i = 0; i < n; ++i) {
-        //     for (int j = 0; j < n; ++j) {
-        //         B[j][n - 1 - i] = A[i][j];
-        //     }
-        // }
-        // // copy back
-        // for (int i = 0; i < n; ++i)
-        //     for (int j = 0; j < n; ++j)
-        //         A[i][j] = B[i][j];
-
-        reverse(A.begin(), A.end());
+    void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin(), matrix.end());
         
-        for (int i = 0; i <= A.size()-1; i++) {
-            for (int j = i + 1; j <= A[i].size()-1; j++) {
-                swap(A[i][j], A[j][i]);
+        for (int i = 0; i <= matrix.size()-1; i++) 
+        {
+            for (int j = i + 1; j <= matrix[i].size()-1; j++) 
+            {
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-    
     }
 };
