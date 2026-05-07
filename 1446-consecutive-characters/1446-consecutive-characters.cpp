3da9cfe811eb3e1c9bb186bab27731ee96f92d94
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-05-07 13:25:49 +0530

    Time: 0 ms (100%), Space: 9.7 MB (15.75%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int count = 1;
+        int res =1;
+
+        for(int i =0;i<s.size()-1;i++)
+        {
+            if(s[i]==s[i+1])
+            {
+                count++;
+                res = max(count,res );
+            }
+            else
+            {
+                count =1;
+            }
+        }
+        return res;
+    }
+};
