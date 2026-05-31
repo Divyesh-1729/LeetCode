232e66b4b1fc8d233d55b60bba7ef3b2879c062e
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-05-31 23:31:27 +0530

    Time: 24 ms (89.78%), Space: 106.8 MB (48.03%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(),asteroids.end());
+
+        for(int m:asteroids)
+        {
+            if(mass<m)
+            {
+                return false;
+            }
+            else if(mass>=1e5)
+            {
+                return true;
+            }
+            mass = mass+m;
+        }
+        return true;
+        
+    }
+};
