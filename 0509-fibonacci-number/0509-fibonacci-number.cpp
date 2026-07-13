a41ce02957bc9edd7b140bd03c5f20189f18dadf
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-07-13 14:20:58 +0530

    Time: 0 ms (100%), Space: 8.4 MB (10.97%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
+    int fin(int n, vector<int>& dp)
+    {
+        if(n<=1)
+        {
+            return n;
+        }
+
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+
+        dp[n]=fin(n-1,dp) +fin(n-2, dp);
+        return dp[n];
+    }
     int fib(int n) {
-        if(n==0)
-        {
-            return 0;
-        }
-        if(n==1)
-        {
-            return 1;
-        }
-        int ans = fib(n-1)+fib(n-2);
-        
-        
+        vector<int>dp(n+1,-1);
+        int ans = fin(n,dp);
         return ans;
+        
     }
 };
