bff564f7e4a04c3234ca1777ebbb2df5d027cfe9
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-07-26 01:24:02 +0530

    Time: 15 ms (9.64%), Space: 27.9 MB (5.02%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int &i:nums)
+        {
+            mp[i]++;
+        }
+
+        for(int i=0;i<=nums.size();i++)
+        {
+            if(mp.find(i)==mp.end())
+            {
+                return i;
+            }
+        }
+        return -1;
+
+    
+    }
+};
