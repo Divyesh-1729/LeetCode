bac2f9af69e32b9e887f2dfa5b510fda9a855716
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-08-08 21:34:39 +0530

    Time: 2 ms (14.4%), Space: 11.7 MB (7.93%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.cpp
+++ 1854-maximum-population-year/1854-maximum-population-year.cpp
@@ -1,30 +1,58 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        vector<int>diff(2051,0);
-        for(auto& it:logs)
-        {
-            int birth = it[0];
-            int death = it[1];
+        // vector<int>diff(2051,0);
+        // for(auto& it:logs)
+        // {
+        //     int birth = it[0];
+        //     int death = it[1];
 
-            diff[birth]++;
-            diff[death]--;
+        //     diff[birth]++;
+        //     diff[death]--;
 
             
 
 
-        }
-        int minyear = INT_MAX;
-        int curr=INT_MIN;
-        int maxi =INT_MIN;
+        // }
+        // int minyear = INT_MAX;
+        // int curr=INT_MIN;
+        // int maxi =INT_MIN;
 
-        for(int year =1950;year<=2050;year++)
+        // for(int year =1950;year<=2050;year++)
+        // {
+        //     curr =curr+diff[year];
+        //     if(curr>maxi)
+        //     {
+        //         maxi = curr;
+        //         minyear = year;
+        //     }
+        // }
+
+        // return minyear; ///DIfference array approach
+
+        vector<vector<int>>events;
+
+        for(auto& log:logs)
         {
-            curr =curr+diff[year];
-            if(curr>maxi)
+            events.push_back({log[0],+1});
+            events.push_back({log[1],-1});
+
+        }
+
+        sort(events.begin(),events.end());
+
+        int currPop =INT_MIN;
+        int maxpop =INT_MIN;
+        int minyear =INT_MAX;
+        
+        for(auto& event:events)
+        {
+            currPop = currPop +event[1];
+
+            if(currPop>maxpop)
             {
-                maxi = curr;
-                minyear = year;
+                maxpop =currPop;
+                minyear =event[0];
             }
         }
 
