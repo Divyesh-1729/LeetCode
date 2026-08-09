e320379c525c1ed7aaef8f3f9d886315bc2b6182
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-08-09 23:50:37 +0530

    Time: 0 ms (100%), Space: 13.2 MB (37.63%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
-        int n=gifts.size();
+        // int n=gifts.size();
         
+        // while(k>0)
+        // {
+        //     sort(gifts.begin(),gifts.end());
+        //     int maxi = gifts[n-1];
+        //     gifts[n-1]=floor(sqrt(maxi));
+        //     k--;
+        // }
+        // long long ans =0;
+        // for(int  i=0;i<gifts.size();i++)
+        // {
+        //     ans = ans+gifts[i];
+        // }
+        // return ans;   BRUTE FORCE AHE PRIORITY QUEUE NE SOLVE KARAVA..
+
+        priority_queue<int>hp;
+        for(int i=0;i<gifts.size();i++)
+        {
+            hp.push(gifts[i]);
+        }
         while(k>0)
         {
-            sort(gifts.begin(),gifts.end());
-            int maxi = gifts[n-1];
-            gifts[n-1]=floor(sqrt(maxi));
+            int num=hp.top();
+            hp.pop();
+            int changed=floor(sqrt(num));
+            hp.push(changed);
+
+            
             k--;
         }
-        long long ans =0;
-        for(int  i=0;i<gifts.size();i++)
+        long long sum =0;
+        while(!hp.empty())
         {
-            ans = ans+gifts[i];
+            sum = sum+hp.top();
+            
+            hp.pop();
         }
-        return ans;
+        return sum;
     }
 };
