d5e781ae997cf1491c9ddf6479cdb445d9dc5563
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-08-09 22:23:57 +0530

    Time: 416 ms (5.84%), Space: 13 MB (86.94%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        int n=gifts.size();
+        
+        while(k>0)
+        {
+            sort(gifts.begin(),gifts.end());
+            int maxi = gifts[n-1];
+            gifts[n-1]=floor(sqrt(maxi));
+            k--;
+        }
+        long long ans =0;
+        for(int  i=0;i<gifts.size();i++)
+        {
+            ans = ans+gifts[i];
+        }
+        return ans;
+    }
+};
