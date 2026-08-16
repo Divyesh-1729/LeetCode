a852c6409dd9ff9f10a6d3113ec4c652ceac3ad4
Divyesh Puranik <147185685+Divyesh-1729@users.noreply.github.com>
2026-08-16 12:06:42 +0530

    Time: 11 ms (27.72%), Space: 102.3 MB (33.49%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,32 +1,46 @@
 class Solution {
 public:
-    int islandPerimeter(vector<vector<int>>& grid) {
-        int perimeter = 0;
-        int rows = grid.size();
-        int cols = grid[0].size();
+int m;
+int n;
+int peri;
 
-        for(int  i = 0 ;i< rows;i++)
+    void dfs(vector<vector<int>>& grid, int i, int j)
+    {
+        if(i>=m||j>=n||j<0||i<0||grid[i][j]==0)
         {
-            for(int j = 0;j<cols;j++)
+            peri++;
+            return;
+        }
+
+        if(grid[i][j]==-1)
+        {
+            return;
+        }
+
+        grid[i][j]=-1;
+
+        dfs(grid,i+1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j-1);
+
+    }
+    int islandPerimeter(vector<vector<int>>& grid) {
+        m = grid.size();
+        n =grid[0].size();
+        peri=0;
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==1)
                 {
-                    perimeter = perimeter +4;
-                
-                if(j+1<cols && grid[i][j+1]==1)
-                {
-                    perimeter = perimeter - 2;
-                }
-                
-                if(i+1<rows && grid[i+1][j]==1)
-                {
-                    perimeter = perimeter - 2;
-                }
+                    dfs(grid,i,j);
+                    return peri;
                 }
             }
-
-
         }
-        return perimeter;
+        return -1;
     }
 };
